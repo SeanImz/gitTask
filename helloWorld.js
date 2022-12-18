3bf1490407ca7609552a749f95c7823f08d08057
Sean <evans.seana@gmail.com>
2022-12-18 21:10:46 +0000

    Added user input

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,7 @@
 console.log("Git is awesome!");
+let userAnswer = prompt("Do you agree? (y/n");
+if ((userAnswer = "y")) {
+  console.log("Glad you agree!");
+} else if ((userAnswer = "n")) {
+  console.log("Sorry you feel that way!");
+}
